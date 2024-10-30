71f7d016057d1c260bf97bba50e04fec855d8f91
Jess <solerj@wncc.edu>
2024-10-30 10:44:44 -0600

    formatting buttons

--- LibraryApp.py
+++ LibraryApp.py
@@ -87,32 +87,32 @@ class LibraryApp:
         # add book button
         # used format from AI code
         self.add_book_button = tk.Button(self.button_frame, text="Add Book", command=self.call_add_book)
-        self.add_book_button.pack(side="top", fill="x", padx=5, pady=15)
+        self.add_book_button.pack(side="top", fill="x", padx=5, pady=7)
         
         # edit book button
         # used format from AI code
         self.edit_book_button = tk.Button(self.button_frame, text="Edit Book", command=self.call_edit_book)
-        self.edit_book_button.pack(side="top", fill="x", padx=5, pady=15)
+        self.edit_book_button.pack(side="top", fill="x", padx=5, pady=7)
         
         # delete book button
         # used format from AI code
         self.delete_book_button = tk.Button(self.button_frame, text="Delete Book", command=self.call_delete_book)
-        self.delete_book_button.pack(side="top", fill="x", padx=5, pady=15)
+        self.delete_book_button.pack(side="top", fill="x", padx=5, pady=7)
         
         # save button
         # used format from AI code
         self.save_button = tk.Button(self.button_frame, text="Save", command=self.call_save_book)
-        self.save_button.pack(side="top", fill="x", padx=5, pady=15)
-        
-        # close app button
-        # used format from AI code
-        self.close_app_button = tk.Button(self.button_frame, text="Close App", command=self.close_app)
-        self.close_app_button.pack(side="top", fill="x", padx=5, pady=15)
+        self.save_button.pack(side="top", fill="x", padx=5, pady=7)
         
         # clear input fields button
         # used format from AI code
         self.clear_input_fields_button = tk.Button(self.button_frame, text="Clear Input Fields", command=self.clear_input_fields)
-        self.clear_input_fields_button.pack(side="top", fill="x", padx=5, pady=15)
+        self.clear_input_fields_button.pack(side="top", fill="x", padx=5, pady=7)
+        
+        # close app button
+        # used format from AI code
+        self.close_app_button = tk.Button(self.button_frame, text="Close App", command=self.close_app)
+        self.close_app_button.pack(side="top", fill="x", padx=5, pady=7)
         
 
     def clear_input_fields(self):
